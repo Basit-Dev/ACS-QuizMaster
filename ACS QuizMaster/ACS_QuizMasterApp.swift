6f3702c8bad687bb846fc63b20444a545ac3df38
Abdul Basit <bas677@icloud.com>
2024-12-31 16:24:44 +0000

    Initial Commit

---
+++ ACS QuizMaster/ACS_QuizMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACS_QuizMasterApp.swift
+//  ACS QuizMaster
+//
+//  Created by Abdul Basit on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ACS_QuizMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
